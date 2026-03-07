50cbf9980debf956f4643bc29e2ee748582c09be
Masum Billah <billahdotdev@gmail.com>
2026-03-07 13:08:50 +0600

    improved bangla

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "digitalizen-v1.4"; // প্রতিবার নতুন build হলে version পরিবর্তন করুন
+const CACHE_NAME = "digitalizen-v1.6"; // প্রতিবার নতুন build হলে version পরিবর্তন করুন
 const urlsToCache = ["/", "/#/free-resources"];
 
 // Install event
