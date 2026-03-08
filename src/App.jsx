b336c1f8d43e5ae8ddfc02e3248f6177dc97449c
Masum Billah <billahdotdev@gmail.com>
2026-03-08 20:48:21 +0600

    remove all landing pages

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,6 @@ import Footer from './components/Footer'
 import InstallButton from './components/InstallButton'
 import FreeResources from './components/FreeResources'
 import Access from './components/Access'   // ← new
-import Beauty from './components/beauty/Beauty'
-import Wellness from './components/wellness/Wellness'
-import Clothing from './components/clothing/Clothing'
-import Personal from './components/personal/Personal'
-import Personal2 from './components/personal2/Personal2'
-import Beauty2 from './components/beauty2/Beauty2'
-
-
 import GrowthHub from './components/GrowthHub'
 
 function MainLayout() {
@@ -54,15 +46,8 @@ export default function App() {
     <HashRouter>
       <Routes>
         <Route path="/"               element={<MainLayout />} />
-        <Route path="/free-resources" element={<FreeResources />} />
+        <Route path="/free" element={<FreeResources />} />
         <Route path="/access"         element={<Access />} />   {/* ← new */}
-        
-        <Route path="/beauty" element={<Beauty />} />
-        <Route path="/beauty2" element={<Beauty2 />} />
-        <Route path="/wellness" element={<Wellness />} />
-        <Route path="/clothing" element={<Clothing />} />
-        <Route path="/personal" element={<Personal />} />
-        <Route path="/personal2" element={<Personal2 />} />
       </Routes>
     </HashRouter>
   )
